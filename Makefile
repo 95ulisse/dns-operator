4084dc1daa56ff66eb30de724e5f99f03d50cd88
Marco Cameriero <me@marcocameriero.net>
2021-02-10 00:57:42 +0100

    Dynamically generate DNS records structure at build time.

--- Makefile
+++ Makefile
@@ -51,6 +51,7 @@ vet:
 
 # Generate code
 generate: controller-gen
+	go generate ./...
 	$(CONTROLLER_GEN) object:headerFile="hack/boilerplate.go.txt" paths="./..."
 
 # Build the docker image
--- pkg/api/v1alpha1/dnsrecord_types.go
+++ pkg/api/v1alpha1/dnsrecord_types.go
@@ -14,12 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//go:generate go run generate_rdata_structs.go
+
 package v1alpha1
 
 import (
-	"fmt"
-	"net"
-
 	"github.com/95ulisse/dns-operator/pkg/dnsname"
 	"github.com/miekg/dns"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -52,51 +51,6 @@ type DNSRecordSpec struct {
 	DeletionPolicy *DeletionPolicy `json:"deletionPolicy,omitempty"`
 }
 
-// DNSRecordData represents the actual contents of a DNS record.
-// Only one of these can be set.
-type DNSRecordData struct {
-	// A record.
-	// +kubebuilder:validation:MinItems=1
-	// +optional
-	A []Ipv4String `json:"a,omitempty"`
-
-	// AAAA record.
-	// +kubebuilder:validation:MinItems=1
-	// +optional
-	AAAA []Ipv6String `json:"aaaa,omitempty"`
-
-	// CNAME record.
-	// +kubebuilder:validation:MinItems=1
-	// +kubebuilder:validation:MaxItems=1
-	// +optional
-	CNAME []NonEmptyString `json:"cname,omitempty"`
-
-	// TXT record.
-	// +kubebuilder:validation:MinItems=1
-	// +optional
-	TXT []string `json:"txt,omitempty"`
-
-	// NS record.
-	// +kubebuilder:validation:MinItems=1
-	// +optional
-	NS []NonEmptyString `json:"ns,omitempty"`
-
-	// MX record.
-	// +kubebuilder:validation:MinItems=1
-	// +optional
-	MX []MXRecordContent `json:"mx,omitempty"`
-}
-
-// MXRecordContent represents the contents of an MX DNS record.
-type MXRecordContent struct {
-	// Name pointed by the MX record.
-	Name NonEmptyString `json:"name"`
-
-	// Priority of the MX record.
-	// +kubebuilder:validation:Minimum=0
-	Priority int64 `json:"priority"`
-}
-
 // DNSRecordStatus defines the observed state of DNSRecord
 type DNSRecordStatus struct {
 	StatusWithConditions `json:",inline"`
@@ -118,40 +72,11 @@ type DNSRecord struct {
 
 // ToRRSet builds a new dns.RR slice equivalent to the Spec of this DNSRecord resource.
 func (record *DNSRecord) ToRRSet() ([]dns.RR, error) {
-
-	// Prepare a common header
-	header := dns.RR_Header{
-		Name:  record.Spec.Name.ToFQDN().String(),
-		Class: dns.ClassINET,
-		Ttl:   3600,
-	}
+	var ttl uint32 = 3600
 	if record.Spec.TTLSeconds != nil {
-		header.Ttl = *record.Spec.TTLSeconds
+		ttl = *record.Spec.TTLSeconds
 	}
-
-	// A record
-	if record.Spec.RData.A != nil {
-		result := make([]dns.RR, 0, 1)
-		for _, ipstr := range record.Spec.RData.A {
-			ip := net.ParseIP(string(ipstr))
-			if ip == nil {
-				return nil, fmt.Errorf("Invalid IPv4 address %s", ipstr)
-			}
-			ip = ip.To4()
-			if ip == nil {
-				return nil, fmt.Errorf("Invalid IPv4 address %s", ipstr)
-			}
-
-			rr := new(dns.A)
-			rr.Hdr = header
-			rr.Hdr.Rrtype = dns.TypeA
-			rr.A = ip
-			result = append(result, rr)
-		}
-		return result, nil
-	}
-
-	return nil, fmt.Errorf("Unsupported DNS record")
+	return toRRSet(record.Spec.Name.ToFQDN().String(), ttl, &record.Spec.RData)
 }
 
 // +kubebuilder:object:root=true
